3781edf0b9f4abbf7394e5e18cf2f4eda2596bcb
Abdi1020 <78614817+abdi1020@users.noreply.github.com>
2021-05-02 15:50:41 +0200

    Menu xml interface

--- app/src/main/java/com/example/handspritsarmbandapp/DashboardActivity.kt
+++ app/src/main/java/com/example/handspritsarmbandapp/DashboardActivity.kt
@@ -2,9 +2,10 @@ package com.example.handspritsarmbandapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.btn_sign_up
 import kotlinx.android.synthetic.main.activity_sign_up.*
@@ -25,4 +26,5 @@ class DashboardActivity : AppCompatActivity() {
             finish()
         }
     }
-}
+
+    }
